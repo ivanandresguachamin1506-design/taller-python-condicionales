8bb714f6195bc63de0c2c8471eb645d7d25235c1
Ivan Guachamin <ivanandresguachamin1506@gmail.com>
2026-02-27 18:04:52 -0500

    Indica si un número es par o impar ademas si es mayor de edad o no

---
+++ Par_o_impar/02_par_impar.py
@@ -0,0 +1,10 @@
+numero = int(input("Ingresa un número entero: "))
+if numero % 2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
